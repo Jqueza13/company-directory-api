923d2606e47341ec0dc45ba7af67ab8adae72682
Jqueza13 <jqueza13@gmail.com>
2024-04-05 17:12:44 -0500

    useAPI line 15 correction

--- src/composables/useAPI.js
+++ src/composables/useAPI.js
@@ -12,7 +12,7 @@ export default function useAPI() {
     const getEmployees = async() => {
         loading.value = true
         if (employees.value.length === 0) {
-            const response = await instance.get('apit/employees/fetch')
+            const response = await instance.get('api/employees/fetch')
             employees.value = response.data
         }
 
